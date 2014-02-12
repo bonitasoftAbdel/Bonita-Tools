c6233109077f1a3193372c80d1e53a70beeb777a
yhon-bonitasoft <yhon.acurio@bonitasoft.com>
2014-02-12 18:29:38 +0100

    First version of code source
    Start version is 1.3

---
+++ src/main/java/org/bonitasoft/utils/BonitaDBTools.java
@@ -0,0 +1,124 @@
+package org.bonitasoft.utils;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Yhon Acurio
+ * 
+ */
+public class BonitaDBTools {
+	/**
+	 * Get the first element from the first column from a table
+	 * 
+	 * @param resultSet
+	 * @return an object
+	 * @throws SQLException
+	 */
+	public static Object getObject(ResultSet resultSet) throws SQLException {
+		Object result = null;
+		if (resultSet.next()) {
+			result = resultSet.getObject(1);
+		}
+		return result;
+	}
+
+	/**
+	 * Get a list of elements from a table
+	 * 
+	 * @param resultSet
+	 * @param columnName
+	 * @return
+	 * @throws SQLException
+	 */
+	public static List<Object> toList(ResultSet resultSet, String columnName)
+			throws SQLException {
+		List<Object> result = new ArrayList<Object>();
+		while (resultSet.next()) {
+			result.add(resultSet.getObject(columnName));
+		}
+		return result;
+	}
+
+	/**
+	 * Get a map of elements from a table (key, value)
+	 * 
+	 * @param resultSet
+	 * @param columnNameForKey
+	 *            Column name of table key
+	 * @param columnNameForLabel
+	 *            Column name of table label
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Map<Object, Object> toKeyValueMap(ResultSet resultSet,
+			String columnNameForKey, String columnNameForLabel)
+			throws SQLException {
+		LinkedHashMap<Object, Object> result = new LinkedHashMap<Object, Object>();
+		while (resultSet.next()) {
+			result.put(resultSet.getObject(columnNameForLabel),
+					resultSet.getObject(columnNameForKey));
+		}
+		return result;
+	}
+
+	/**
+	 * Get a table into a list of map format
+	 * 
+	 * @param resultSet
+	 * @return A list of map. A row will be an element of the list. Ex:
+	 *         [[column1name
+	 *         :row1column1value,column2name:row1column2value],[column1name
+	 *         :row2column1value,column2name:row2column2value]]
+	 * @throws SQLException
+	 */
+	public static List toListOfMap(ResultSet resultSet) throws SQLException {
+		List result = new ArrayList<Map>();
+		ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+		int columnsNumber = resultSetMetaData.getColumnCount();
+
+		while (resultSet.next()) {
+			LinkedHashMap<Object, Object> map = new LinkedHashMap<Object, Object>();
+			if (columnsNumber > 0) {
+				for (int i = 0; i < columnsNumber; i++) {
+					map.put(resultSetMetaData.getColumnName(i + 1),
+							resultSet.getObject(i + 1));
+				}
+			}
+			result.add(map);
+		}
+		return result;
+	}
+
+	/**
+	 * Get a table into a list of list format
+	 * 
+	 * @param resultSet
+	 * @return A list of map. A row will be an element of the list. Ex:
+	 *         [[row1column1value
+	 *         ,row1column2value],[row2column1value,row2column2value]]
+	 * @throws SQLException
+	 */
+	public static List toListOfList(ResultSet resultSet) throws SQLException {
+		List result = new ArrayList<Map>();
+		ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+		int columnsNumber = resultSetMetaData.getColumnCount();
+
+		while (resultSet.next()) {
+			List list = new ArrayList<Object>();
+			if (columnsNumber > 0) {
+				for (int i = 0; i < columnsNumber; i++) {
+					list.add(resultSet.getObject(i + 1));
+				}
+			}
+			result.add(list);
+		}
+		return result;
+	}
+
+}
---
+++ src/main/java/org/bonitasoft/utils/BonitaURLTools.java
@@ -0,0 +1,370 @@
+package org.bonitasoft.utils;
+
+import org.bonitasoft.engine.api.APIAccessor;
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstanceNotFoundException;
+import org.bonitasoft.engine.bpm.process.ProcessDefinition;
+import org.bonitasoft.engine.bpm.process.ProcessDefinitionNotFoundException;
+
+/**
+ * @author Yhon Acurio
+ * @see <a
+ *      href="http://documentation.bonitasoft.com/accessing-bonita-bpm-portal-and-forms-url">Accessing
+ *      Bonita BPM Portal and forms by URL </a>
+ */
+public class BonitaURLTools {
+	public enum FormMode {
+		app, form;
+	}
+
+	public static String APPLICATION_URL = "/bonita/console/homepage";
+
+	/**
+	 * @param protocol
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * @param activityInstanceId
+	 * @param formMode
+	 *            app or form
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of task
+	 * @return URL to access to a human task form
+	 * @throws ProcessDefinitionNotFoundException
+	 * @throws ActivityInstanceNotFoundException
+	 */
+	@Deprecated
+	public static String getHumanTaskURL(String protocol, String host,
+			long port, long activityInstanceId, FormMode formMode,
+			APIAccessor apiAccessor) throws ActivityInstanceNotFoundException,
+			ProcessDefinitionNotFoundException {
+		return getHumanTaskURL(protocol, host, port, activityInstanceId,
+				formMode, apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param protocol
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * @param formName
+	 *            a task form name is generated as
+	 *            PROCESS_NAME--PROCESS_VERSION-TASK_NAME. Ex: Pool1--1.0--Step1
+	 * @param activityInstanceId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * @return URL to access to a human task form
+	 * @throws ProcessDefinitionNotFoundException
+	 * @throws ActivityInstanceNotFoundException
+	 */
+	@Deprecated
+	public static String getHumanTaskURL(String protocol, String host,
+			long port, long activityInstanceId, FormMode formMode,
+			ProcessAPI processAPI) throws ActivityInstanceNotFoundException,
+			ProcessDefinitionNotFoundException {
+		StringBuffer buffer = new StringBuffer("");
+		buffer.append(protocol);
+		buffer.append("://");
+		buffer.append(host);
+		buffer.append(":");
+		buffer.append(port);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form#form=");
+		buffer.append(getHumanTaskFormName(activityInstanceId, processAPI));
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&task=");
+		buffer.append(activityInstanceId);
+
+		return buffer.toString();
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * 
+	 * @param activityInstanceId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * @return URL to access to a human task form
+	 * @throws ProcessDefinitionNotFoundException
+	 * @throws ActivityInstanceNotFoundException
+	 */
+
+	public static String getHumanTaskURL(String server,
+			long activityInstanceId, FormMode formMode, ProcessAPI processAPI)
+			throws ActivityInstanceNotFoundException,
+			ProcessDefinitionNotFoundException {
+		StringBuffer buffer = new StringBuffer(server);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form#form=");
+		buffer.append(getHumanTaskFormName(activityInstanceId, processAPI));
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&task=");
+		buffer.append(activityInstanceId);
+		buffer.append("&assignTask=true");
+
+		return buffer.toString();
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * @param activityInstanceId
+	 * 
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of task
+	 * @return URL to access to a human task form in APP mode
+	 * @throws ProcessDefinitionNotFoundException
+	 * @throws ActivityInstanceNotFoundException
+	 */
+	public static String getHumanTaskURL(String server,
+			long activityInstanceId, APIAccessor apiAccessor)
+			throws ActivityInstanceNotFoundException,
+			ProcessDefinitionNotFoundException {
+		return getHumanTaskURL(server, activityInstanceId, FormMode.app,
+				apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param protocol
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of process
+	 * @return URL to access to a process initialization form
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	@Deprecated
+	public static String getProcessURL(String protocol, String host, long port,
+			long processDefinitionId, FormMode formMode, APIAccessor apiAccessor)
+			throws ProcessDefinitionNotFoundException {
+		return getProcessURL(protocol, host, port, processDefinitionId,
+				formMode, apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param protocol
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * 
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * @return URL to access to a process initialization form
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	@Deprecated
+	public static String getProcessURL(String protocol, String host, long port,
+			long processDefinitionId, FormMode formMode, ProcessAPI processAPI)
+			throws ProcessDefinitionNotFoundException {
+		StringBuffer buffer = new StringBuffer("");
+		buffer.append(protocol);
+		buffer.append("://");
+		buffer.append(host);
+		buffer.append(":");
+		buffer.append(port);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form&form=");
+		buffer.append(getProcessFormName(processDefinitionId, processAPI));
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&process=");
+		buffer.append(processDefinitionId);
+		return buffer.toString();
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * 
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * @return URL to access to a process initialization form
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	public static String getProcessURL(String server, long processDefinitionId,
+			FormMode formMode, ProcessAPI processAPI)
+			throws ProcessDefinitionNotFoundException {
+		StringBuffer buffer = new StringBuffer(server);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form&form=");
+		buffer.append(getProcessFormName(processDefinitionId, processAPI));
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&process=");
+		buffer.append(processDefinitionId);
+		return buffer.toString();
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * @param processDefinitionId
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of process
+	 * @return URL to access to a process initialization form in APP mode
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	public static String getProcessURL(String server, long processDefinitionId,
+			FormMode formMode, APIAccessor apiAccessor)
+			throws ProcessDefinitionNotFoundException {
+		return getProcessURL(server, processDefinitionId, FormMode.app,
+				apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param protocole
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of process
+	 * @return URL to access to a process initialization form in autologin mode
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	@Deprecated
+	public static String getProcessURLWithAutologin(String protocol,
+			String host, long port, long processDefinitionId,
+			FormMode formMode, APIAccessor apiAccessor)
+			throws ProcessDefinitionNotFoundException {
+
+		return getProcessURLWithAutologin(protocol, host, port,
+				processDefinitionId, formMode, apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param protocol
+	 *            Enter http, https, ...
+	 * @param host
+	 *            Enter your hostname
+	 * @param port
+	 *            Enter your port number
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * 
+	 * @return URL to access to a process initialization form in autologin mode
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	@Deprecated
+	public static String getProcessURLWithAutologin(String protocol,
+			String host, long port, long processDefinitionId,
+			FormMode formMode, ProcessAPI processAPI)
+			throws ProcessDefinitionNotFoundException {
+		String formName = getProcessFormName(processDefinitionId, processAPI);
+		StringBuffer buffer = new StringBuffer("");
+		buffer.append(protocol);
+		buffer.append("://");
+		buffer.append(host);
+		buffer.append(":");
+		buffer.append(port);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form&autologin=");
+		buffer.append(formName);
+		buffer.append("#form=");
+		buffer.append(formName);
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&process=");
+		buffer.append(processDefinitionId);
+		return buffer.toString();
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * @param processDefinitionId
+	 * 
+	 * @param apiAccessor
+	 *            APIAccessor used to get details of process
+	 * @return URL to access to a process initialization form in autologin and
+	 *         APP mode
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	public static String getProcessURLWithAutologin(String server,
+			long processDefinitionId, APIAccessor apiAccessor)
+			throws ProcessDefinitionNotFoundException {
+
+		return getProcessURLWithAutologin(server, processDefinitionId,
+				FormMode.app, apiAccessor.getProcessAPI());
+	}
+
+	/**
+	 * @param server
+	 *            Enter http://localhost:8080 or http://localhost or
+	 *            https://localhost:8081
+	 * @param processDefinitionId
+	 * @param formMode
+	 *            app or form
+	 * @param processAPI
+	 * 
+	 * @return URL to access to a process initialization form in autologin mode
+	 * @throws ProcessDefinitionNotFoundException
+	 */
+	public static String getProcessURLWithAutologin(String server,
+			long processDefinitionId, FormMode formMode, ProcessAPI processAPI)
+			throws ProcessDefinitionNotFoundException {
+		String formName = getProcessFormName(processDefinitionId, processAPI);
+		StringBuffer buffer = new StringBuffer(server);
+		buffer.append(APPLICATION_URL);
+		buffer.append("?ui=form&autologin=");
+		buffer.append(formName);
+		buffer.append("#form=");
+		buffer.append(formName);
+		buffer.append("$entry&mode=");
+		buffer.append(formMode.toString());
+		buffer.append("&process=");
+		buffer.append(processDefinitionId);
+		return buffer.toString();
+	}
+
+	private static String getHumanTaskFormName(long activityInstanceId,
+			ProcessAPI processAPI) throws ActivityInstanceNotFoundException,
+			ProcessDefinitionNotFoundException {
+		ActivityInstance activityInstance = processAPI
+				.getActivityInstance(activityInstanceId);
+
+		return getProcessFormName(activityInstance.getProcessDefinitionId(),
+				processAPI) + "--" + activityInstance.getName();
+	}
+
+	private static String getProcessFormName(long processDefinitionId,
+			ProcessAPI processAPI) throws ProcessDefinitionNotFoundException {
+		ProcessDefinition processDefinition = processAPI
+				.getProcessDefinition(processDefinitionId);
+		return processDefinition.getName() + "--"
+				+ processDefinition.getVersion();
+	}
+}
